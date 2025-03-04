f1c984713f5ca88bed983a42b41cfd7092642215
vavil <vavilonskiy99@mail.ru>
2025-03-04 15:27:30 +0500

    fix ci file v3

--- bank/bank/settings.py
+++ bank/bank/settings.py
@@ -11,7 +11,7 @@ load_dotenv()
 # Quick-start development settings - unsuitable for production
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = getenv('DJANGO_SECRET_KEY', 'asfaekhg8923htgnipnsklfc')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv(
@@ -19,7 +19,7 @@ DEBUG = getenv(
     True
 )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
